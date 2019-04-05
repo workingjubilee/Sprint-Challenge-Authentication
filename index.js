c790f5ff51d3473a735bedde9d6664870b9692dc
workingjubilee <workingjubilee@gmail.com>
2019-04-05 09:35:10 -0700

    Eliminate all placeholders for environment variables.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // load .env variables
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 3300;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
 });
