318b2e308b73190d1a141ecbd52a4d1ea1661a0e
workingjubilee <workingjubilee@gmail.com>
2019-04-05 10:43:04 -0700

    Crude implementation of jokes GET and mount

--- webjokes/src/Jokes.js
+++ webjokes/src/Jokes.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Jokes(props) {
-  return <h1>...</h1>;
+  const [jokes, setJokes] = useState();
+
+
+  useEffect(
+    () => {
+      const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImhvcGUiLCJpYXQiOjE1NTQ0ODI3MTcsImV4cCI6MTU1NDU2OTExN30.pR3FdHzoM3z0PysxWnGqGEKyi4DduYOgMEZpYPrRlek";
+      const jokeGrab = async () => {
+        let theJokes = await axios.get('http://localhost:7777/api/jokes', { headers: {
+          Authorization: `${token}`
+        }});
+        setJokes(theJokes.data);
+      };
+      jokeGrab()
+    },
+    []
+  );
+
+  return (
+    <code>
+      <pre>
+        {JSON.stringify(jokes, null, 2)}
+      </pre>
+    </code>);
 }
 
 export default Jokes;
